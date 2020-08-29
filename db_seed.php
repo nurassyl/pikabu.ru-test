f8b8c15fdd9f40511acc98b6beb78bad7d05535f
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2020-08-29 09:19:47 +0500

    created_at and updated_at columns created for tables.

--- db_seed.php
+++ db_seed.php
@@ -2,8 +2,8 @@
 
 require_once './db.php';
 
-$result = $db->query('CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(15))');
-$result = $db->query('CREATE TABLE IF NOT EXISTS user_relations (user_id BIGINT UNSIGNED NOT NULL, relation_id BIGINT UNSIGNED NOT NULL, type SMALLINT(1) UNSIGNED NOT NULL)');
+$result = $db->query('CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(15), created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)');
+$result = $db->query('CREATE TABLE IF NOT EXISTS user_relations (user_id BIGINT UNSIGNED NOT NULL, relation_id BIGINT UNSIGNED NOT NULL, type SMALLINT(1) UNSIGNED NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)');
 $result = $db->query('CREATE INDEX `idx-user_relations-user_id` ON user_relations (user_id)');
 $result = $db->query('ALTER TABLE user_relations ADD FOREIGN KEY `fk-user_relations-user_id` (user_id) REFERENCES `users` (id) ON DELETE CASCADE');
 $result = $db->query('CREATE INDEX `idx-user_relations-relation_id` ON user_relations (relation_id)');
